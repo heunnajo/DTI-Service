061f77642c4f5478aa737927acf56a1a0140ae9f
조흔나 <gms950831@gmail.com>
2024-01-02 14:44:26 +0900

    Update TaxinvoiceRepositoryTest.java

--- dti/src/test/java/sb/dti/TaxinvoiceRepositoryTest.java
+++ dti/src/test/java/sb/dti/TaxinvoiceRepositoryTest.java
@@ -1,7 +1,36 @@
 package sb.dti;
 
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
 import static org.junit.Assert.*;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class TaxinvoiceRepositoryTest {
+    @Autowired
+    TaxinvoiceRepository taxinvoiceRepository;
 
+    @Test
+    @Transactional
+    @Rollback(false)
+    public void testTaxinvoice() throws Exception{
+        //given
+        Taxinvoice taxinvoice = new Taxinvoice();
+        taxinvoice.setUserID("id1234");
+
+        //when
+        Long savedId = taxinvoiceRepository.save(taxinvoice);
+        Taxinvoice findTaxinvoice = taxinvoiceRepository.find(savedId);
+
+        //then
+        Assertions.assertThat(findTaxinvoice.getConversationId().equals(taxinvoice.getConversationId()));
+        Assertions.assertThat(findTaxinvoice.getCorpNum().equals(taxinvoice.getCorpNum()));
+    }
 }
