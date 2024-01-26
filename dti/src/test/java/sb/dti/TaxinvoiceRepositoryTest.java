63dc7ba925639060a13d918fc6a5153c4d176f15
조흔나 <gms950831@gmail.com>
2024-01-26 16:57:52 +0900

    Update TaxinvoiceRepositoryTest.java

--- dti/src/test/java/sb/dti/TaxinvoiceRepositoryTest.java
+++ dti/src/test/java/sb/dti/TaxinvoiceRepositoryTest.java
@@ -4,6 +4,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -13,6 +14,7 @@ import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureTestDatabase
 public class TaxinvoiceRepositoryTest {
     @Autowired
     TaxinvoiceRepository taxinvoiceRepository;
